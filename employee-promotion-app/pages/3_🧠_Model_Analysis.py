32a3b464a4974821b371c2f78df447a38a5ee10a
dwiputri-git <dwip5704@gmail.com>
2025-11-07 08:20:55 +0700

    Update 3_🧠_Model_Analysis.py

--- employee-promotion-app/pages/3_🧠_Model_Analysis.py
+++ employee-promotion-app/pages/3_🧠_Model_Analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import numpy as np
 
 st.set_page_config(page_title="Model Analysis", layout="wide")
 
